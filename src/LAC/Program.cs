2febe256d67922aef21f66673e3a8565cb8abdbd
Note <120763388+ForgottenNote@users.noreply.github.com>
2022-12-16 23:36:23 -0500

    update

--- src/LAC/Program.cs
+++
@@ -1,8 +0,0 @@
-﻿using Microsoft.Extensions.Hosting;
-using LAC;
-
-await Host.CreateDefaultBuilder(args)
-    .UseSystemd()
-    .ConfigureLogging(Startup.ConfigureLogging)
-    .ConfigureServices(Startup.ConfigureServices)
-    .RunConsoleAsync().ConfigureAwait(false);
